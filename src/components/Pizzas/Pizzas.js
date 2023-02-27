184108bc2ea6441b9c14bab7dad12dbdf8633c13
Vladyslava <vladatsyupyak@gmail.com>
2023-02-27 17:51:14 +0200

    feat: fetch pizzas from api

--- src/components/Pizzas/Pizzas.js
+++ src/components/Pizzas/Pizzas.js
@@ -1,10 +1,22 @@
 import {pizzasDb} from "../../assets/pizzasDb";
 import {PizzaBlock} from "./PizzaBlock/PizzaBlock";
+import {useEffect, useState} from "react";
 
 export const Pizzas = ()=>{
+    const [pizzas, setPizzas] = useState([])
+    useEffect(()=>{
+        fetch("https://63fccae08ef914c5559fa9a6.mockapi.io/items")
+            .then((res)=>{
+              return res.json()
+            })
+            .then((res)=>{
+                console.log(res)
+                setPizzas(res)
+            })
+    }, [])
     return           <div className="content__items">
 
-        {pizzasDb.pizzas.map((el)=>{
+        {pizzas.map((el)=>{
             return   <PizzaBlock key={el.id} pizzaInfo={el}/>
         })}
     </div>
